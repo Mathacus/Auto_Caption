9ac01e17514d020e2e616b7aece2470248c3f926
mathacus <mattymccutcheon@gmail.com>
2025-07-06 17:53:36 +1200

    touched up

--- backend/run.py
+++ backend/run.py
@@ -1,14 +1,13 @@
 from pipeline.extract_audio import mp4_to_mp3, isolate_vocals
 from pipeline.transcribe_audio import transcribe
-import asyncio
 
 def run(input, output, vocal_output):
     mp4_to_mp3(input, output)
     isolate_vocals(output)
     transcribe(vocal_output)
 
-input_file = "./files/input.mp4"
-output_file = "./files/output.mp3"
-vocal_output_file = "./files/output_vocals.mp3"
+input_file = "input.mp4"
+output_file = "output.mp3"
+vocal_output_file = "output_vocals.mp3"
 
 run(input_file, output_file, vocal_output_file)
